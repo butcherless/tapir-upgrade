8d082cc3a25fa06531505d013371e23589abfbbc
Carlos Martin <cmarting@gmail.com>
2022-07-06 11:35:40 +0200

    test renovate (docker local)

--- basic/build.gradle.kts
+++ basic/build.gradle.kts
@@ -21,7 +21,7 @@ dependencies {
     // Use the Kotlin JDK 8 standard library.
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("io.arrow-kt:arrow-core:1.1.2")
+    implementation("io.arrow-kt:arrow-core:1.1.0")
 
     // Use the Kotlin test library.
     testImplementation("org.jetbrains.kotlin:kotlin-test")
